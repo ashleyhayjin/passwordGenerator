ef6ffe6c69f895da278d78c97463354cac0fe75d
Ashley Lee <ashleyhayjinlee@gmail.com>
2021-01-14 22:02:23 -0600

    Homework

--- Develop/script.js
+++ Develop/script.js
@@ -7,54 +7,121 @@ var generateBtn = document.querySelector("#generate");
 //Password options Y/N for numbers, special characters, and letters
 
 var numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var letterList = ["a", "b", "c" , "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCaseList = ["A","B","C","D","E","F","G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var letterList = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCaseList = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 var specialList = [",", "!", "?", "@", "*", "#", "/", "&"];
 var finalList = [];
+var finalPassword = "";
 
 
 function getPasswordOptions() {
+
   var characterAmount = prompt(
     "How many characters would you like in your password? (Min: 8 Max: 128)?"
   );
+
   var numberChar = confirm("Do you want number characters?");
   var letterChar = confirm("Do you want lowercase letter characters?");
   var upperCaseChar = confirm("Do you want capital letters?");
   var specialChar = confirm("Do you want special characters?");
+}
 
-  if (numberChar !== true && letterChar !== true && upperCaseChar !== true && specialChar !== true) {
+
+
+function randomizer() {
+  for (i = 0; i < characterAmount; i++) {
+    var characterPick = finalList[Math.floor(Math.random() * characterAmount)];
+    console.log(characterPick);
+  }
+}
+
+
+function generatePassword() {
+  if (
+    numberChar !== true &&
+    letterChar !== true &&
+    upperCaseChar !== true &&
+    specialChar !== true
+  ) {
     alert("You must select one");
   }
 
-  if(numberChar === true) {
+  if (numberChar === true) {
     var finalList = [numberList];
   }
-  if(numberChar === true && letterChar === true) {
+  if (numberChar === true && letterChar === true) {
     var finalList = [numberList + letterList];
   }
-  if (numberChar === true && letterChar  === true && upperCaseChar === true) {
+  if (numberChar === true && letterChar === true && upperCaseChar === true) {
     var finalList = [numberList + letterList + upperCaseList];
   }
-  if (numberChar === true && letterChar === true && upperCaseChar === true && specialChar === true) {
+  if (
+    numberChar === true &&
+    letterChar === true &&
+    upperCaseChar === true &&
+    specialChar === true
+  ) {
     var finalList = [numberList + letterList + upperCaseList + specialList];
   }
 }
 
-function randomizer() {
-  for (i=0; i < characterAmount.length; i++) {
-    var characterPick = passwordFinal[Math.floor(Math.random() * characterAmount.length);
-      
-    ];
-  }
-}
+generatePassword();
+randomizer();
 
-getPasswordOptions();
-
-// function generatePassword() {
-//   if (numberChar === true && letterChar === true && upperCaseChar === true && specialChar === true) {
-//     var passwordOptions = numberList + letterList + specialList + upperCaseList;
-//   }
-// }
 
 //Function will generate
 function writePassword() {
